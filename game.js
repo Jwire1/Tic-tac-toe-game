fa40de583a443fcf63db4591ae8a78e0fb48fcc9
Jwire1 <58442944+Jwire1@users.noreply.github.com>
2019-12-21 12:50:42 +0000

    Update game.js

--- game.js
+++ game.js
@@ -1,3 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
 function Square(props) {
   return (
     React.createElement("button", { className: "square", onClick: props.onClick },
